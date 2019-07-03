2c1ec96e4dd4eb7c012ca4ff2b17842086d3414d
madyk1 <k.mady@emc.com>
2019-07-03 13:29:03 -0400

    Download Excel

--- src/app/cycle-members/cycle-members.component.ts
+++ src/app/cycle-members/cycle-members.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../Services/user.service';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-cycle-members',
@@ -11,16 +13,18 @@ export class CycleMembersComponent implements OnInit {
 
   users = [];
   members = [];
-  urlParam : any;
-  cycleId : any;
+  urlParam: any;
+  cycleId: any;
   membersFetched = false;
+  EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+  EXCEL_EXTENSION = '.xlsx';
 
   constructor(private userService: UserService,
-    private route: ActivatedRoute) { 
-      this.route.params.subscribe( params => this.urlParam = params );
-      var temp = Object.values(this.urlParam)[0];
-      this.cycleId = parseInt(temp.toString(), 10)
-    }
+    private route: ActivatedRoute) {
+    this.route.params.subscribe(params => this.urlParam = params);
+    var temp = Object.values(this.urlParam)[0];
+    this.cycleId = parseInt(temp.toString(), 10)
+  }
 
   ngOnInit() {
     this.getUsers();
@@ -37,4 +41,20 @@ export class CycleMembersComponent implements OnInit {
     }
     console.log(this.members)
   }
+
+
+  async download() {
+
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.members);
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    this.saveAsExcelFile(excelBuffer, "Members");
+
+  }
+
+  private saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], { type: this.EXCEL_TYPE });
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + this.EXCEL_EXTENSION);
+  }
+
 }
